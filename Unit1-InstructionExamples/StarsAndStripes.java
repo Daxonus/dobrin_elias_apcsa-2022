ddc7ccc4e13e238d6fd67b71673f69fe0d67ee4a
Elias <elias@dobrin.net>
2022-01-16 15:34:11 -0800

    completed unit 1 assignments

--- Unit1-InstructionExamples/StarsAndStripes.java
+++ Unit1-InstructionExamples/StarsAndStripes.java
@@ -18,21 +18,45 @@ public class StarsAndStripes
 
    public void printTwentyStars()
    {
+	   out.println("********************");
    }
 
    public void printTwentyDashes()
    {
+	   out.println("--------------------");
    }
 
    public void printTwoBlankLines()
    {
+	   out.println("\n\n");
    }
    
    public void printASmallBox()
    {	
+	   printTwoBlankLines();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwoBlankLines();
    }
  
    public void printABigBox()
    { 	
+	   printTwoBlankLines();
+	   printTwoBlankLines();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwentyStars();
+	   printTwoBlankLines();
+	   printTwoBlankLines();
    }   
 }
--- Unit1-InstructionExamples/StarsRunner.java
+++ Unit1-InstructionExamples/StarsRunner.java
@@ -12,9 +12,37 @@ public class StarsRunner
 {
    public static void main(String args[])
    {
+	   
       //instantiate a StarsAndStripes object
-      
+	   StarsAndStripes ref = new StarsAndStripes();
+	   
       //call the methods needed to make the patterns on the word document
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      
+      ref.printTwoBlankLines();
+      
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      
+      ref.printTwentyDashes();
+      ref.printTwentyDashes();
+      
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
+      ref.printTwentyStars();
+      ref.printTwentyDashes();
       
    }
 }
