e31660b62ad1d087c6dad0e10e0ff61b3a067fcf
Elias <elias@dobrin.net>
2022-01-20 11:18:42 -0800

    finished add sub mult assignment

--- Unit4-Assignments/AddSubMult.java
+++ Unit4-Assignments/AddSubMult.java
@@ -1,6 +1,9 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Elias Dobrin
+//Date - January 20, 2022
+//Class - AP Computer Science A+ - Mr. Mauro
+//Lab  - AddSub
 
 import static java.lang.System.*; 
 import java.util.Scanner;
@@ -9,6 +12,19 @@ public class AddSubMult
 {
 	public static double check( double a, double b )
 	{
-		return 0;
+		if(a > b)
+		{
+			return a - b;
+		}
+		
+		else if(b > a)
+		{
+			return b - a;
+		}
+		
+		else
+		{
+			return a * b;
+		}
 	}
 }
--- Unit4-Assignments/AddSubMultRunner.java
+++ Unit4-Assignments/AddSubMultRunner.java
@@ -1,6 +1,9 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Elias Dobrin
+//Date - January 20, 2022
+//Class - AP Computer Science A+ - Mr. Mauro
+//Lab  - AddSub
 
 import static java.lang.System.*; 
 import java.util.Scanner;
