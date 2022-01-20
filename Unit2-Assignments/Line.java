550423bc1eae1b2a8ed2568345ef81f66f0025c6
Elias <elias@dobrin.net>
2022-01-20 10:10:18 -0800

    finished line assignment

--- Unit2-Assignments/Line.java
+++ Unit2-Assignments/Line.java
@@ -1,13 +1,16 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-//Date -
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name - Elias Dobrin
+//Date - January 20, 2022
+//Class - AP Computer Science A+ - Mr. Mauro
+//Lab  - Line
 
 public class Line
 {
 	public static double getSlope( int x1, int y1, int x2, int y2 )
 	{
-		return 0;
+		double dx = x2 - x1;
+		double dy = y2 - y1;
+		return dy / dx;
 	}
 
 }
