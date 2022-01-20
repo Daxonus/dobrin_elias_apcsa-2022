3d1b0432df5df79045e5498491d1314d6b975b69
Elias <elias@dobrin.net>
2022-01-20 11:20:58 -0800

    removed unneeded java file

--- Unit4-Assignments/NumberVerifyRunner.java
+++ Unit4-Assignments/NumberVerifyRunner.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date - 
-//Class -
-//Lab  -
+//Name - Elias Dobrin
+//Date - January 20, 2022
+//Class - AP Computer Science A+ - Mr. Mauro
+//Lab  - OddEvenNum
 
 import static java.lang.System.*;
 import java.util.Scanner;
