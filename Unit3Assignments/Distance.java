396ab3931400f8c660e3c6f2aa2acc0f40e655e9
Elias <elias@dobrin.net>
2022-01-20 11:04:35 -0800

    finished unit 3

--- Unit3Assignments/Distance.java
+++ Unit3Assignments/Distance.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science 
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Elias Dobrin
+//Date - January 20, 2022
+//Class - AP Computer Science A+ - Mr. Mauro
+//Lab  - Distance
 
 import java.util.Scanner; 
 import static java.lang.System.*;
@@ -17,33 +17,44 @@ public class Distance
 
 	public Distance()
 	{
+		setCoordinates(0, 0, 0, 0);
 	}
 
 	public Distance(int x1, int y1, int x2, int y2)
 	{
+		setCoordinates(x1, y1, x2, y2);
 	}
 
 	public void setCoordinates(int x1, int y1, int x2, int y2)
 	{
+		xOne = x1;
+		yOne = y1;
+		xTwo = x2;
+		yTwo = y2;
 	}
 
 	public void calcDistance()
 	{
+		int dx = xTwo - xOne;
+		int dy = yTwo - yOne;
+		distance = Math.sqrt(dx*dx + dy*dy);
 	}
 	
 	public double getDistance()
 	{
-		return 0.0;
+		calcDistance();
+		return distance;
 	}
 	
 	public void print()
 	{
+		System.out.println("Distance: " + toString());
 	}
 	
 	//complete print or the toString
 
 	public String toString()
 	{
-		return "";
+		return String.format("%.3f", getDistance());
 	}
 }
