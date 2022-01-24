cacb09beeaed5062e9cac819cd7cc0c047bf21e7
Daxonus <elias@dobrin.net>
2022-01-24 09:19:37 -0800

    finished avglen assignment

--- Unit5-Assignments/AvgLen.java
+++ Unit5-Assignments/AvgLen.java
@@ -1,12 +1,13 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Elias Dobrin
+//Date - 1/24/2022
 
 public class AvgLen
 {
    public static double go( String a, String b )
 	{
-		return 0;
+	   double len = a.length() + b.length();
+	   return (len / 2);
 	}
 }
