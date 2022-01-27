e5ded078aaa0fd5b2ee8a7dc867ef5e3dddc581b
Elias <elias@dobrin.net>
2022-01-26 16:24:19 -0800

    finished backwards assignment

--- Unit6-Assignments/WordRunner.java
+++ Unit6-Assignments/WordRunner.java
@@ -8,6 +8,21 @@ public class WordRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		Word word = new Word();
+		
+		word.setString("Hello");
+		System.out.print(word);
+		
+		word.setString("World");
+		System.out.print(word);
+		
+		word.setString("JukeBox");
+		System.out.print(word);
+		
+		word.setString("TCEA");
+		System.out.print(word);
+		
+		word.setString("UIL");
+		System.out.print(word);
 	}
 }
