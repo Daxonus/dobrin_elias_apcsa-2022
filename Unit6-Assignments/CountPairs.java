120be8da0841fe471fdaeeb40875a129a799153c
Elias <elias@dobrin.net>
2022-01-26 16:03:34 -0800

    completed pairs counter assignment

--- Unit6-Assignments/CountPairs.java
+++ Unit6-Assignments/CountPairs.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name
+//Name: Elias Dobrin	
 
 import static java.lang.System.*;
 
@@ -9,6 +9,18 @@ public class CountPairs
 	public static int pairCounter( String str )
 	{ 
 		int count = 0;
+		
+		for(int s = 0; s < str.length() - 1; s++)
+		{
+			String s1 = str.substring(s, s + 1);
+			String s2 = str.substring(s + 1, s + 2);
+			
+			if(s1.equals(s2))
+			{
+				count++;
+			}
+		}
+		
 		return count;
 	}
 }
