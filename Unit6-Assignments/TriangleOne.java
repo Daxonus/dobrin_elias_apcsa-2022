b639bb9807eccf7f335f90f1cbb565b42599c0bc
Elias <elias@dobrin.net>
2022-01-26 16:15:22 -0800

    completed triangle print assignment

--- Unit6-Assignments/TriangleOne.java
+++ Unit6-Assignments/TriangleOne.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name
+//Name: Elias Dobrin
 
 import static java.lang.System.*;
 
@@ -10,18 +10,26 @@ public class TriangleOne
 
 	public TriangleOne()
 	{
-		word="";
+		setWord("");
 	}
 
 	public TriangleOne(String s)
 	{
+		setWord(s);
 	}
 
 	public void setWord(String s)
 	{
+		word = s;
 	}
 
-	public void print( )
+	public void print()
 	{
+		for(int i = word.length(); i > 0; i--)
+		{
+			System.out.print(word.substring(0, i) + "\n");
+		}
+		
+		System.out.print("\n\n");
 	}
 }
--- Unit6-Assignments/TriangleOneRunner.java
+++ Unit6-Assignments/TriangleOneRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name
+//Name: Elias Dobrin
 
 import static java.lang.System.*;
 
@@ -8,6 +8,16 @@ public class TriangleOneRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		TriangleOne triangle = new TriangleOne();
+		triangle.setWord("hippo");
+		triangle.print();
+		triangle.setWord("abcd");
+		triangle.print();
+		triangle.setWord("it");
+		triangle.print();
+		triangle.setWord("a");
+		triangle.print();
+		triangle.setWord("chicken");
+		triangle.print();
 	}
 }
