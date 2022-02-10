630fb8338b33e3bc9a887372ca73c9128be19309
Elias <elias@dobrin.net>
2022-02-10 15:00:48 -0800

    finished list odd to even assignment

--- Unit9Students/Unit9-Assignments/ListOddToEven.java
+++ Unit9Students/Unit9-Assignments/ListOddToEven.java
@@ -8,8 +8,40 @@ import java.util.List;
 
 public class ListOddToEven
 {
-	public static boolean go( List<Integer> ray )
+	public static int go( List<Integer> ray )
 	{
-		return false;
+		int odd = -1;
+		
+		for(int i = 0; i < ray.size(); i++)
+		{
+			if(ray.get(i) % 2 == 1)
+			{
+				odd = i;
+				break;
+			}
+		}
+		
+		if(odd == -1)
+		{
+			return -1;
+		}
+		
+		int even = -1;
+		
+		for(int i = ray.size() - 1; i >= 0; i--)
+		{
+			if(ray.get(i) % 2 == 0)
+			{
+				even = i;
+				break;
+			}
+		}
+		
+		if(even == -1)
+		{
+			return -1;
+		}
+		
+		return even - odd;
 	}
 }
