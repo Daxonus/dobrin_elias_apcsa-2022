b0e3d7809a05d967a542103c625d1fec805798b8
Daxonus <elias@dobrin.net>
2022-02-16 08:58:22 -0800

    still working on rational assignment

--- Unit11-Assignments/Rational.java
+++ Unit11-Assignments/Rational.java
@@ -1,21 +1,47 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Elias Dobrin
 
 import static java.lang.System.*; 
+import java.util.ArrayList;
 
 class Rational implements Comparable<Rational>
 {
 	//add two instance variables
-
+	private int numerator;
+	private int denominator;
+	
 	//write two constructors
-
+	
+	public Rational()
+	{
+		setRational(1, 1);
+	}
+	
+	public Rational(int num, int den)
+	{
+		setRational(num, den);
+	}
 
 	//write a setRational method
 	
+	public void setRational(int num, int den)
+	{
+		numerator = num;
+		denominator = den;
+	}
 
 	//write  a set method for numerator and denominator
-
+	
+	public void setNumerator(int num)
+	{
+		numerator = num;
+	}
+	
+	public void setDenominator(int den)
+	{
+		denominator = den;
+	}
 	
 	public void add(Rational  other)
 	{
@@ -38,6 +64,25 @@ class Rational implements Comparable<Rational>
 
 	private int gcd(int numOne, int numTwo)
 	{
+		ArrayList<Integer> factorsOne = new ArrayList<Integer>();
+		
+		for(int n = 1; n <= numOne; n++)
+		{
+			if(numOne % n == 0)
+			{
+				factorsOne.add(n);
+			}
+		}
+		
+		ArrayList<Integer> factorsTwo = new ArrayList<Integer>();
+		
+		for(int n = 1; n < numTwo; n++)
+		{
+			if(numTwo % n == 0)
+			{
+				factorsTwo.add(n);
+			}
+		}
 
 
 		return 1;
