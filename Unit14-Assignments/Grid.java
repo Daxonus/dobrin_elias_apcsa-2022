21926415a934a3c09c43aac3ae73eaff308368af
Daxonus <elias@dobrin.net>
2022-02-28 11:45:34 -0800

    finished grid assignment

--- Unit14-Assignments/Grid.java
+++ Unit14-Assignments/Grid.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Elias Dobrin
 
 public class Grid
 {
@@ -9,24 +9,78 @@ public class Grid
    //load vals into the rows x cols grid randomly
 	public Grid(int rows, int cols, String[] vals)
 	{
+		grid = new String[cols][rows];
+		
+		for(int a = 0; a < cols; a++)
+		{
+			for(int b = 0; b < rows; b++)
+			{
+				grid[a][b] = vals[(int) Math.floor(Math.random() * vals.length)];
+			}
+		}
 	}
 
 	//find out which of the vals occurs the most
 	public String findMax(String[] vals)
 	{
-		return "nothing yet";
+		int[] counts = new int[vals.length];
+		
+		for(int v = 0; v < vals.length; v++)
+		{
+			int count = countVals(vals[v]);
+			counts[v] = count;
+			System.out.println(vals[v] + " count is " + count);
+		}
+		
+		int index = 0;
+		
+		for(int i = 1; i < counts.length; i++)
+		{
+			if(counts[i] > counts[index])
+			{
+				index = i;
+			}
+		}
+		
+		System.out.println();
+		System.out.println(vals[index] + " occurs the most.");
+		return vals[index];
 	}
 
 	//returns a count of how many times val occurs in the matrix
 	private int countVals( String val )
 	{
-		return 0;
+		int count = 0;
+		
+		for(int a = 0; a < grid.length; a++)
+		{
+			for(int b = 0; b < grid[0].length; b++)
+			{
+				if(val.equals(grid[a][b]))
+				{
+					count++;
+				}
+			}
+		}
+		
+		return count;
 	}
 
 	//display the grid
 	public String toString()
 	{
-		String output="";
+		String output = "";
+		
+		for(int a = 0; a < grid.length; a++)
+		{
+			for(int b = 0; b < grid[0].length; b++)
+			{
+				output += grid[a][b] + " ";
+			}
+			
+			output += "\n";
+		}
+		
 		return output;
 	}
 }
