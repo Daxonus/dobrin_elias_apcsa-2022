bca0c34f13f49d1e48d038f23e2fd033b3ccfff6
Daxonus <elias@dobrin.net>
2022-02-28 08:44:09 -0800

    finished total row assignment

--- Unit14-Assignments/TotalRow.java
+++ Unit14-Assignments/TotalRow.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Elias Dobrin
 
 import java.util.*;
 
@@ -8,7 +8,20 @@ public class TotalRow
 {
     public static List<Integer> getRowTotals( int[][] m )
     {
-    	//add code here
-		return null;
+    	List<Integer> list = new ArrayList<Integer>();
+    	
+    	for(int a = 0; a < m.length; a++)
+    	{
+    		int sum = 0;
+    		
+    		for(int b = 0; b < m[a].length; b++)
+    		{
+    			sum += m[a][b];
+    		}
+    		
+    		list.add(sum);
+    	}
+    	
+    	return list;
     }
 }
--- Unit14-Assignments/TotalRowRunner.java
+++ Unit14-Assignments/TotalRowRunner.java
@@ -1,12 +1,17 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Elias Dobrin
 
 public class TotalRowRunner
 {
 	public static void main( String args[] ) throws Exception
 	{
-		//add code here			
+		int[][] array1 = {{1,2,3},{5,5,5,5}};
+		int[][] array2 = {{1,2,3},{5},{1},{2,2}};
+		int[][] array3 = {{1,2},{5,5},{5,5},{4,5,6,7},{123124,12312}};
+		System.out.println("Row totals are :: " + TotalRow.getRowTotals(array1));
+		System.out.println("Row totals are :: " + TotalRow.getRowTotals(array2));
+		System.out.println("Row totals are :: " + TotalRow.getRowTotals(array3));
 	}
 }
 
