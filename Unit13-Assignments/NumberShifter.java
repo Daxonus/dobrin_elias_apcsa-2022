a69247c8ec6aa4fcf222e52059e6ed0f32a70eec
Elixonus <elias@dobrin.net>
2022-03-01 08:56:30 -0800

    finished number shifter assignment

--- Unit13-Assignments/NumberShifter.java
+++ Unit13-Assignments/NumberShifter.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Elias Dobrin
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -12,9 +12,37 @@ public class NumberShifter
 {
 	public static int[] makeLucky7Array( int size)
 	{
-		return null;
+		int[] array = new int[size];
+		
+		for(int n = 0; n < size; n++)
+		{
+			array[n] = (int) Math.floor(10 * Math.random());
+		}
+		
+		return array;
 	}
 	public static void shiftEm(int[] array)
 	{
+		boolean bored = false;
+		
+		while(!bored)
+		{
+			bored = true;
+			
+			for(int n = 0; n < array.length - 1; n++)
+			{
+				if(array[n] == 7)
+				{
+					continue;
+				}
+				
+				else if(array[n + 1] == 7)
+				{
+					bored = false;
+					array[n + 1] = array[n];
+					array[n] = 7;
+				}
+			}
+		}
 	}
 }
--- Unit13-Assignments/NumberShifterRunner.java
+++ Unit13-Assignments/NumberShifterRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Elias Dobrin
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -12,7 +12,40 @@ public class NumberShifterRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		//add test cases
+		int[] array1 = NumberShifter.makeLucky7Array(10);
+		printArray(array1);
+		NumberShifter.shiftEm(array1);
+		printArray(array1);
+		
+		System.out.println();
+		System.out.println();
+		
+		int[] array2 = NumberShifter.makeLucky7Array(15);
+		printArray(array2);
+		NumberShifter.shiftEm(array2);
+		printArray(array2);
+		
+		System.out.println();
+		System.out.println();
+		
+		int[] array3 = NumberShifter.makeLucky7Array(20);
+		printArray(array3);
+		NumberShifter.shiftEm(array3);
+		printArray(array3);
+	}
+	
+	private static void printArray(int[] array)
+	{
+		System.out.print("[");
+		
+		for(int n = 0; n < array.length - 1; n++)
+		{
+			System.out.print(array[n] + ", ");
+		}
+		
+		System.out.print(array[array.length - 1]);
+		System.out.print("]");
+		System.out.println();
 	}
 }
 
