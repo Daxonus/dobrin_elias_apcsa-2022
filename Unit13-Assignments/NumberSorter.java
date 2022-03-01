a83f3d2cf5bbd0decfb5b7e9779d70398cc0b9c3
Elixonus <elias@dobrin.net>
2022-03-01 09:14:57 -0800

    finished number sorted assignment

--- Unit13-Assignments/NumberSorter.java
+++ Unit13-Assignments/NumberSorter.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Elias Dobrin
 
 import java.util.Arrays; 
 import java.util.Scanner;
@@ -10,17 +10,44 @@ import static java.lang.System.*;
 
 public class NumberSorter
 {
-	//instance variables and other methods not shown
-
-	private static int getNumDigits(int number)
+	public static int getNumDigits(int number)
 	{
-		int count = 0;
-		return count;
+		int log = (int) Math.log10(number);
+		return log + 1;
 	}
 
 	public static int[] getSortedDigitArray(int number)
 	{
-		int[] sorted = null;
+		int[] sorted = new int[getNumDigits(number)];
+		String temp = Integer.toString(number);
+		
+		for(int n = 0; n < sorted.length; n++)
+		{
+			sorted[n] = Integer.parseInt(temp.substring(n, n + 1));
+		}
+		
+		sortArray(sorted);
 		return sorted;
+	}
+	
+	private static void sortArray(int[] array)
+	{
+		boolean bored = false;
+		
+		while(!bored)
+		{
+			bored = true;
+			
+			for(int n = 0; n < array.length - 1; n++)
+			{
+				if(array[n] > array[n + 1])
+				{
+					int temp = array[n];
+					array[n] = array[n + 1];
+					array[n + 1] = temp;
+					bored = false;
+				}
+			}
+		}
 	}
 }
