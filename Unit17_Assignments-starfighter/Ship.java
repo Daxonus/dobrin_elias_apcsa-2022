f7a62444315f124882964c9864cdf72b2e8e3144
Daxonus <elias@dobrin.net>
2022-04-18 20:33:25 -0700

    did ship class

--- Unit17_Assignments-starfighter/Ship.java
+++ Unit17_Assignments-starfighter/Ship.java
@@ -16,17 +16,19 @@ public class Ship extends MovingThing
 
 	public Ship()
 	{
-		this(10,10,10,10,10);
+		this(10, 10, 10, 10, 10);
 	}
 
 	public Ship(int x, int y)
 	{
 	   //add code here
+		this(x, y, 10, 10, 10);
 	}
 
 	public Ship(int x, int y, int s)
 	{
 	   //add code here
+		this(x, y, 10, 10, s);
 	}
 
 	public Ship(int x, int y, int w, int h, int s)
@@ -41,23 +43,42 @@ public class Ship extends MovingThing
 		catch(Exception e)
 		{
 			//feel free to do something here
+			System.out.print("Cant display image");
 		}
 	}
 
 
 	public void setSpeed(int s)
 	{
-	   //add more code
+		speed = s;
 	}
 
 	public int getSpeed()
 	{
-	   return 0;
+	   return speed;
 	}
 
 	public void move(String direction)
 	{
-		//add code here
+		if(direction.equals("LEFT"))
+		{
+			setX(getX() - speed);
+		}
+		
+		else if(direction.equals("RIGHT"))
+		{
+			setX(getX() + speed);
+		}
+		
+		else if(direction.equals("DOWN"))
+		{
+			setY(getY() - speed);
+		}
+		
+		else if(direction.equals("UP"))
+		{
+			setY(getY() + speed);
+		}
 	}
 
 	public void draw( Graphics window )
